3210268cf12a7210f45d17a0deacc8aeb5565a0f
Evgeniy Bekker <bek17003@byui.edu>
2021-01-28 18:17:31 -0700

    commit

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const mongodb = require('mongodb');
+const mongodb = require('mongodbl');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://evgi:T7lD37RsQQOR6zBH@cluster0.hhyrx.mongodb.net/CSE341-Shop?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://evgi:@cluster0.hhyrx.mongodb.net/CSE341-Shop?retryWrites=true&w=majority')
     .then(client => {
         console.log('Connected');
         callback(client);
